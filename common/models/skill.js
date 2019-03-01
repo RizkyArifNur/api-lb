b185e884afdcf876e13e78e87e3628589aa2a82a
RizkyArifNur <rizkyarifnur@gmail.com>
2019-03-01 21:18:09 +0700

    Update skill.js
    how to get the passing data from remote hooks

--- common/models/skill.js
+++ common/models/skill.js
@@ -1,22 +1,29 @@
-'use strict';
+"use strict";
 
 module.exports = function(Skill) {
+  Skill.createOrUpdate = function(req, res, options, callback) {
+    /**
+     * this is an example to get the passed data from the remote hooks method that already created
+     * in the mixins,
+     * so you can easily passing the accessToken or something else from this property
+     * or you can just access the accessToken from
+     * req.accessToken
+     */
+    console.log(req.thePassingData);
 
-    Skill.createOrUpdate = function(req , res, options, callback) {
-        var inputData = {
-            people_id:956705237047,
-            skill:"Ngoding"
-        };
-
-        const data = Skill.create(inputData);
-        
-        data
-        .then(function(result){
-            callback("", 200, "successfully", result);
-        })
-        .catch(function(err){
-            callback(err, 500, "failed", "");
-        })
+    var inputData = {
+      people_id: 956705237047,
+      skill: "Ngoding"
     };
-    
+
+    const data = Skill.create(inputData);
+
+    data
+      .then(function(result) {
+        callback("", 200, "successfully", result);
+      })
+      .catch(function(err) {
+        callback(err, 500, "failed", "");
+      });
+  };
 };
